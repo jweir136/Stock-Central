814ff931c3401eb2ca7c4fd98df0844dc21ed922
psdev30 <pszjukalkar30@gmail.com>
2021-12-01 03:13:55 -0500

    updated test script

--- rdb/test_script.sql
+++ rdb/test_script.sql
@@ -10,6 +10,7 @@ SELECT fk_user_id_2 FROM friends WHERE fk_user_id_1 = 1;
 -- SELECT * FROM friends JOIN posts ON friends.fk_user_id_2 = posts.fk_user_id JOIN likes ON likes.fk_user_id = posts.fk_user_id WHERE fk_user_id_1 = 1 AND posts.created_at > (NOW() - INTERVAL 7 DAY) ORDER BY likes.num_likes DESC LIMIT 10;
 SELECT * FROM friends JOIN posts ON friends.fk_user_id_2 = posts.fk_user_id JOIN likes ON likes.fk_post_id = posts.post_id WHERE friends.fk_user_id_1 = 1 AND posts.created_at > (NOW() - INTERVAL 7 DAY) AND likes.num_likes > 10 LIMIT 10;
 
+SELECT * FROM stock_central.users WHERE first_name = 'Prateek';
 
 SELECT * FROM users;
 select * from posts;

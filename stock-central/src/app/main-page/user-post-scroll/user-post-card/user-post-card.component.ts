5ea61cad16b404babde7ef3eee6da293ae9ef333
Greg Doyle <18gdoyle@gmail.com>
2021-12-08 00:21:13 -0500

    date and lowercase stock entries fixed

--- stock-central/src/app/main-page/user-post-scroll/user-post-card/user-post-card.component.ts
+++ stock-central/src/app/main-page/user-post-scroll/user-post-card/user-post-card.component.ts
@@ -1,4 +1,3 @@
-import { parseHostBindings } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 import { LikeService } from 'src/app/services/like.service';
 
@@ -19,6 +18,14 @@ export class UserPostCardComponent implements OnInit {
   numOfLikes = 0;
   liked = false;
   postCreationDate = '';
+  month = 0;
+  day = 0;
+  year = 0;
+  hour = 0;
+  minute = 0;
+  second = 0;
+  time = 0;
+  AMorPM = ''
 
   constructor(private likeService: LikeService) { }
 
@@ -30,6 +37,7 @@ export class UserPostCardComponent implements OnInit {
       }
     }
     this.postCreationDate = this.post.created_at;
+    this.setTime();
     this.message = this.post.message_content
     this.username = this.post.username
     this.user = this.post;
@@ -58,4 +66,31 @@ export class UserPostCardComponent implements OnInit {
     this.numOfLikes--;
   }
 
+  setTime() {
+    this.hour = parseInt((this.postCreationDate.split('T')[1]).split(':')[0]);
+    this.minute = parseInt(this.postCreationDate.split(':')[1]);
+    this.second = parseInt(this.postCreationDate.split(':')[2]);
+    this.day = parseInt((this.postCreationDate.split('-')[2]).split('T')[0]);
+    this.month = parseInt(this.postCreationDate.split('-')[1])-1;
+    this.year = parseInt(this.postCreationDate.split('-')[0])
+    var d = new Date(this.year, this.month, this.day, this.hour, this.minute, this.second, 0);
+    var timestamp = Math.floor(d.getTime()/1000.0)-18000;
+    const date = new Date();
+    timestamp = (timestamp) - (date.getTimezoneOffset())*60;
+    let calculatedDate = new Date(0);
+    calculatedDate.setUTCSeconds(timestamp);
+    this.day = calculatedDate.getDate();
+    this.month = calculatedDate.getMonth() + 1;
+    this.year = calculatedDate.getFullYear();
+    this.hour = calculatedDate.getHours();
+    this.minute = calculatedDate.getMinutes();
+    this.AMorPM += (this.hour >= 12) ? "PM" : "AM";
+    if (this.hour > 0 && this.hour <= 12) {
+      this.hour = this.hour;
+    } else if (this.hour > 12) {
+      this.hour = (this.hour - 12);
+    } else if (this.hour == 0) {
+      this.hour = 12;
+    }
+  }
 }
